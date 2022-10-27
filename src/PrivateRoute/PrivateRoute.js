f37a0bc7c50423f229c3f674b6741b0a380a9d68
Mahedi Hasan <mahedihasandu1@gmail.com>
2022-10-27 21:27:42 +0600

    serversiteupdate

--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext)
     const location = useLocation();
     if (loading) {
-        return <progress className="progress w-56"></progress>
+        return <progress className="progress w-56 text-center"></progress>
     };
     if (user && user.uid) {
         return children;
